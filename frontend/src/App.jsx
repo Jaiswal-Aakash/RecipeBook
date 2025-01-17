77a7f91a5f24925a52db498ba723147964a936f5
Akash Jaiswal <realakashjaiswal@gmail.com>
2025-01-17 22:09:23 +0530

    changes made in json file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import RecipeDetails from "./pages/RecipeDetail/RecipeDetails";
 import Favorites from "./pages/Favourite";
 import Navbar from "./components/Navbar/Navbar";
